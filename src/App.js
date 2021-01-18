2d58a900a2feb2e9f5663e4851046d7b732d3464
tylerdahl123 <dahlgren15@gmail.com>
2021-01-17 22:23:53 -0800

    trying something

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from "react"
 import './App.css';
 import Header from "./components/Header"
 import Wrapper from "./components/Wrapper";
+import Search from "./pages/Search"
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 
 function App(){
   return (
+    <Router>
   <div>
     <Wrapper>
     <Header />
+    <Route exact path="/search" component={Search} />
     </Wrapper>
   </div>
+  </Router>
 )
 }
   
